513109f0742c2b3a2780365a48f415fec920a234
sakkus42 <seyitakkus44@gmail.com>
2024-02-07 16:06:09 +0300

    08/09

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,8 +6,8 @@ PmergeMe::PmergeMe(PmergeMe const& pm) {
 }
 PmergeMe& PmergeMe::operator=(PmergeMe const& pm) {
     if (this != &pm){
-        this->list1 = pm.list1;
-        this->list2 = pm.list2;
+        this->list = pm.list;
+        this->vector = pm.vector;
     }
     return *this;
 }
@@ -17,19 +17,92 @@ PmergeMe::PmergeMe(int ac, char** argv) {
     if (checkArgs(ac, argv))
         throw std::runtime_error("Error: Invalid argument");
     fillLists(ac, argv);
+    mergeSort(list);
+    mergeSort(vector);
+
+    std::cout << "After:    ";
+    for (std::list<int>::iterator el = list.begin(); el != list.end(); el++)
+        std::cout << *el << " ";
+    std::cout << std::endl;
+    std::cout << "Time to process a range of " << list.size() <<
+    " elements with std::list   : " << std::fixed << std::setprecision(4) 
+    << this->lst_time * 1'000'000 << " us" << std::endl;
+    std::cout << "Time to process a range of " << list.size() <<
+    " elements with std::vector : " << std::fixed << std::setprecision(4) 
+    << this->vc_time * 1'000'000 << " us" << std::endl;
+    
+}
+
+template<typename Iterator>
+void PmergeMe::insertionSort(Iterator begin, Iterator end) {
+    Iterator it, key, prev;
+    for (it = std::next(begin); it != end; it++) {
+        key = it;
+        prev = std::prev(it);
+        while (key != begin && *prev > *key) {
+            std::swap(*prev, *key);
+            prev = std::prev(prev);
+            key = std::prev(key);
+        }
+    }
+}
+
+
+void  PmergeMe::mergeSort(std::list<int>& list) {
+    std::clock_t start = clock();
+    if (list.size() < 5) {
+        insertionSort(list.begin(), list.end());
+        std::clock_t end = clock();
+        this->lst_time = (double)(end - start) / CLOCKS_PER_SEC;
+        return;
+    }
+    std::list<int>::iterator it_begin = list.begin();   
+    std::list<int>::iterator it_end = list.end();
+    std::list<int>::iterator it_mid = list.begin();
+    std::advance(it_mid, list.size() / 2);
+    std::list<int> leftSubList(it_begin, it_mid);
+    std::list<int> rightSubList(it_mid, it_end);
+    mergeSort(leftSubList);
+    mergeSort(rightSubList);
+    std::merge(leftSubList.begin(), leftSubList.end(), rightSubList.begin(), rightSubList.end(), list.begin());
+    std::clock_t end = clock();
+    this->lst_time = (double)(end - start) / CLOCKS_PER_SEC;
+}
+
+void  PmergeMe::mergeSort(std::vector<int>& vector) {
+    std::clock_t start = clock();
+    if (vector.size() < 5) {
+        insertionSort(vector.begin(), vector.end());
+        std::clock_t end = clock();
+        this->vc_time = (double)(end - start) / CLOCKS_PER_SEC;
+        return;
+    }
+    std::vector<int>::iterator it_begin = vector.begin();   
+    std::vector<int>::iterator it_end = vector.end();
+    std::vector<int>::iterator it_mid = vector.begin() + (vector.end() - vector.begin()) / 2;
+    std::vector<int> leftSubVector(it_begin, it_mid);
+    std::vector<int> rightSubVector(it_mid, it_end);
+    mergeSort(leftSubVector);
+    mergeSort(rightSubVector);
+    std::merge(leftSubVector.begin(), leftSubVector.end(), rightSubVector.begin(), rightSubVector.end(), vector.begin());
+    std::clock_t end = clock();
+    this->vc_time = (double)(end - start) / CLOCKS_PER_SEC;
 }
 
 void PmergeMe::fillLists(int ac, char** argv) {
+    std::stringstream ss;
     for (int i = 0; i < ac; i++) {
         try {
             int nbr = std::stoi(argv[i]);
-            list1.push_back(nbr);
-            list2.push_back(nbr);
+            list.push_back(nbr);
+            vector.push_back(nbr);
+            ss << nbr << " ";
         } catch(const std::exception& e) {
             std::cerr << e.what() << std::endl;
             exit(0);
         }
     }
+    std::cout << "Before:   " << ss.str() << std::endl;
 }
 
 bool PmergeMe::checkArgs(int ac, char** argv) {
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 #include <sstream>
+#include <ctime>
+#include <iomanip>
 
 class PmergeMe
 {
     private:
-        std::list<unsigned int> list1;
-        std::vector<unsigned int> list2;
-
+        std::list<int>      list;
+        std::vector<int>    vector;
+        double              vc_time;
+        double              lst_time;
+        
         bool    isDigit(std::string str);
         bool    checkArgs(int ac, char** argv);
         void    fillLists(int ac, char** argv);
+
+        template<typename Iterator>
+        void    insertionSort(Iterator begin, Iterator end);   
+        void    mergeSort(std::list<int>& lst1);
+        void    mergeSort(std::vector<int>& lst2);
     public:
         PmergeMe();
         PmergeMe(int ac, char** argv);
